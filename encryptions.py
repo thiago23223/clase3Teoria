33ab2cc53f10ac12b61667999043de9693c9c2ef
thiago23223 <lucenatato2006@gmail.com>
2025-03-30 16:19:23 -0300

    añado descripcion de funciones y un comentario

--- encryptions.py
+++ encryptions.py
@@ -1,10 +1,12 @@
 def inc_ascii(letter):
+    """ funcion q recibe un caracter y me incrementa en 1 su ascii"""
     letter = ord(letter)
     letter += 1
     letter = chr(letter)
     return letter
 
 def encrypt (phrase):
+    """ funcion q reciba una frase y devuelve esa frase encriptada"""
     encrypted_phrase = ""
     for words in phrase.split():
         encrypted_word = ""
@@ -14,8 +16,9 @@ def encrypt (phrase):
     return encrypted_phrase.strip()
 
 def encrypt_lambda(phrase):
+    """ funcion q reciba una frase y devuelve esa frase encriptada"""
     encrypted_phrase = ""
     for words in phrase.split():
-        encrypted_word =  "".join(map(lambda letter: chr(ord(letter)+1),words))
+        encrypted_word =  "".join(map(lambda letter: chr(ord(letter)+1),words)) #le aplica la funcion lambda a cada uno de los caracteres de la palabra
         encrypted_phrase += encrypted_word + " "
     return encrypted_phrase.strip()
